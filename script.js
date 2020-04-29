8b5a09b36035b65f0c5e7a6de31bbf83471735cb
Zahir Ismat <zahir.s.ismat@gmail.com>
2020-04-29 03:54:23 -0400

    Uploaded initial files for code quiz app

---
+++ script.js
@@ -0,0 +1,10 @@
+function afterFirst() {
+  document.getElementById('firstQuestion').innerHTML="When creating a variable, first we must type...";
+  document.getElementById('firstOption').innerHTML="Create variable";
+  document.getElementById('secondOption').innerHTML="var";
+  document.getElementById('thirdOption').innerHTML="Require variable";
+  document.getElementById('fourthOption').innerHTML="variable";
+
+} 
+
+
